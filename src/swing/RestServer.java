c6bf383be17a19d9511bb5fcea5a76a5c3ed2951
sfkhalili <fatima.khalili2012@gmail.com>
2017-10-30 16:06:28 +0330

    authentication add to server

--- src/swing/RestServer.java
+++ src/swing/RestServer.java
@@ -2,70 +2,146 @@ package swing;
 
 import java.security.NoSuchAlgorithmException;
 import java.sql.SQLException;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.xml.ws.WebServiceContext;
+import javax.xml.ws.handler.MessageContext;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.hibernate.mapping.Map;
+
+import com.mchange.v2.codegen.bean.Property;
 
 @Path("/FinalProject")
 public class RestServer {
+	static{
+	BasicConfigurator.configure();}
+	static  Logger log = Logger.getLogger(RestServer.class);
+
+	
+	static HashMap<String, User> map = new HashMap<String, User>();
+
+	// @GET
+	// @Produces("application/xlm")
+	// public String getData(@Context HttpServletRequest request){
+	// String ip = request.getRemoteAddr();
+	// System.out.println("Client IP = " + ip);
+	// return ip;
+	// }
+
+	// login
+	@Path("/login")
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response validation(User u, @Context HttpServletRequest request) throws ClassNotFoundException,
+			InstantiationException, IllegalAccessException, SQLException, NoSuchAlgorithmException {
+
+		String ip = request.getRemoteAddr();
+		int role = hibernate.getRole(u.getUsername());
+		u.setRole(role);
+		map.put(ip, u);
+		boolean flag = hibernate.validation(u);
+		return Response.status(200).entity(flag).build();
+	}
 
 	// insert the contact
 	@Path("/addcontact")
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response CreateContact(Contact c)
+	public Response CreateContact(Contact c, @Context HttpServletRequest request)
 			throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException {
-		
-		hibernate.Insertcontact(c);
-		return Response.status(200).entity("insert successful contact with" ).build();
+		String ip = request.getRemoteAddr();
+		User u = map.get(ip);
+		if (u.getRole() == 4|| u.getRole() == 1 || u.getRole() == 2 || u.getRole() == 3) {// no
+																							// guest
+
+			hibernate.Insertcontact(c);
+			log.info("save successful");
+			return Response.status(200).entity("insert successfuly").build();
+		} else {
+			return Response.status(200).entity("you not access").build();
+		}
 	}
 
-	 //show all contacts
+	// show all contacts
 	@Path("/showcontact")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public List<Contact> selectContact()throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException {
+	public List<Contact> selectContact(@Context HttpServletRequest request)
+			throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException {
 
-		List<Contact> tickets = hibernate.selectContacts();
-		return tickets;
+		String ip = request.getRemoteAddr();
+		User u = map.get(ip);
+		if (u != null) {
+			if (u.getRole() == 4 || u.getRole() == 1 || u.getRole() == 2 || u.getRole() == 3) {
+				List<Contact> contacts = hibernate.selectContacts();
+				log.debug("get successful");
+				return contacts;
+			} else {
+				return null;
+			}
+		} else {
+			List<Contact> contacts = hibernate.selectContacts();
+			log.info("get successful");
+			return contacts;
+		}
+
 	}
 
 	// update by id
 	@Path("/updatecontact/{id}")
 	@PUT
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response updateContact(Contact c, @PathParam("id") String id)
+	public Response updateContact(Contact c, @PathParam("id") String id, @Context HttpServletRequest request)
 			throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException {
 		String result;
-		if (hibernate.UpdateContact(c, Integer.parseInt(id))) {
+		String ip = request.getRemoteAddr();
+		User u = map.get(ip);
+		if (u.getRole() == 4 || u.getRole() == 1 || u.getRole() == 2) {// managers
+			if (hibernate.UpdateContact(c, Integer.parseInt(id))) {
 
-			result = "The contact with id " + id + " update successfully.";
-			System.out.println(result);
+				result = "The contact with id " + id + " update successfully.";
+				System.out.println(result);
+			} else {
+				result = "The contact with id " + id + " which you want to update does'nt exist.";
+
+			}
+			return Response.status(200).entity(result).build();
 		} else {
-			result = "The contact with id " + id + " which you want to update does'nt exist.";
-			System.out.println(result);
+			return Response.status(200).entity("you not access").build();
 		}
-		return Response.status(200).entity(result).build();
+
 	}
 
 	// Delete by id contact
 	@Path("/deletecontact/{id}")
 	@DELETE
-	public Response DeleteContact(@PathParam("id") String id)
+	public Response DeleteContact(@PathParam("id") String id, @Context HttpServletRequest request)
 			throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException {
 		String result;
-		if (hibernate.DeleteContact(Integer.parseInt(id))) {
 
-			result = "The contact with id " + id + " deleted successfully.";
-			System.out.println(result);
+		String ip = request.getRemoteAddr();
+		User u = map.get(ip);
+		if (u.getRole() == 4 || u.getRole() == 1 || u.getRole() == 2) {// managers
+			if (hibernate.DeleteContact(Integer.parseInt(id))) {
+
+				result = "The contact with id " + id + " deleted successfully.";
+				System.out.println(result);
+			} else {
+				result = "The contact with id " + id + " which you want to delete does'nt exist.";
+				System.out.println(result);
+			}
+			return Response.status(200).entity(result).build();
 		} else {
-			result = "The contact with id " + id + " which you want to delete does'nt exist.";
-			System.out.println(result);
+			return Response.status(200).entity("you not access").build();
 		}
-		return Response.status(200).entity(result).build();
 	}
 
 	// select by id
@@ -78,53 +154,49 @@ public class RestServer {
 		return hibernate.selectContact(Integer.parseInt(id));
 
 	}
-	
+
 	// insert the user
 	@Path("/user")
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response Createuser(User u)
-			throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException, NoSuchAlgorithmException {
-		
-		hibernate.Insertuser(u);
-		return Response.status(200).entity("insert successful customer" ).build();
+	public Response Createuser(User u, @Context HttpServletRequest request) throws ClassNotFoundException,
+			InstantiationException, IllegalAccessException, SQLException, NoSuchAlgorithmException {
+
+		String ip = request.getRemoteAddr();
+		u = map.get(ip);
+		if (u.getRole() == 4 || u.getRole() == 1) {// just role 1
+
+			hibernate.Insertuser(u);
+			log.info("save successful");
+			return Response.status(200).entity("insert successfuly customer").build();
+		} else {
+			return Response.status(200).entity("you not access").build();
+		}
+
 	}
-	
+
 	// update by id
 	@Path("/updateuser/{username}")
 	@PUT
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response updateuser(User c, @PathParam("username") String name)
+	public Response updateuser(User c, @PathParam("username") String name, @Context HttpServletRequest request)
 			throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException {
-		String result="";
-		if (hibernate.Updateuser(c, name)) {
+		String result = "";
+		String ip = request.getRemoteAddr();
+		User u = map.get(ip);
+		if (u.getRole() == 4 || u.getRole() == 1) {// managertotal
+			if (hibernate.Updateuser(c, name)) {
 
-			result = "The person with username " + name + " update successfully.";
-			System.out.println(result);
+				result = "The person with username " + name + " update successfully.";
+				System.out.println(result);
+			} else {
+				result = "The person with username " + name + " which you want to update does'nt exist.";
+				System.out.println(result);
+			}
+			return Response.status(200).entity(result).build();
 		} else {
-			result = "The person with username " + name + " which you want to update does'nt exist.";
-			System.out.println(result);
+			return Response.status(200).entity("you not access").build();
 		}
-		return Response.status(200).entity(result).build();
 	}
-	
-	@Path("/save")
-	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	public Response saveperson(User u)
-			throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException {
-		String result;
-		int role=hibernate.saveuser(u.getUsername());
-		result="the username have a role by number of"+role;
-		return Response.status(200).entity(result).build();
-	}
-	@Path("/login")
-	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	public Response validation(User u)
-			throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException, NoSuchAlgorithmException {
-		
-		boolean flag=hibernate.validation(u);
-		return Response.status(200).entity(flag ).build();
-	}
+
 }
